d8a9e7c9ba0647ccbf805b6695239beca338fb56
Nathan Strout <SpatialStrout@users.noreply.github.com>
2016-07-22 16:55:50 -0700

    cleaned up for general use

--- samples/updateUserGroups.py
+++ samples/updateUserGroups.py
@@ -1,12 +1,10 @@
 import sys
 sys.path.append(r"..\..")
 from agoTools.admin import Admin
-if sys.argv[1]=='css':
-    agoAdmin = Admin("user_name_here",password="password")
-    groups=['group_ID_1', 'group_ID_2', 'group_ID_3', 'group_ID_4']
-if sys.argv[1]=='sb':
-    agoAdmin = Admin("user_name_here",password="password")
-    groups = ['group_ID_1', 'group_ID_2']   # Enter <groupIDs> of groups to which you want to add new users
+
+agoAdmin = Admin("user_name_here",password="password")
+groups=['group_ID_1', 'group_ID_2', 'group_ID_3', 'group_ID_4']
+
 # User parameters:
 orgUsers= agoAdmin.getUsers()
 
